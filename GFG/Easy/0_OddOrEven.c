7632c8c65c60da737d0fd35bb5efe2cd2544032c
Penkey Mahitha <mahithapenkey@gmail.com>
2026-09-26 20:26:00 +0530

    Sync GFG - Odd or Even (Easy)

---
+++ GFG/Easy/0_OddOrEven.c
@@ -0,0 +1,16 @@
+/**
+ * Problem Link : https://practice.geeksforgeeks.org/problems/odd-or-even3618/1
+ * Platform     : GFG
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int isEven(int n) {
+    // code here
+    if(n%2==0)
+        return 1;
+    else
+        return 0;
+}
